ee7b5b9a3ab1b88ba5cf2c1196e5e12cfe5c879b
TheChanRProject <garpchoo@gmail.com>
2020-02-17 02:54:19 -0800

    Commiting my changes

--- src/players/overwatch_players_eda.py
+++ src/players/overwatch_players_eda.py
@@ -10,6 +10,11 @@ data_path = getcwd() + "/data/ow_players"
 
 ow_player_dict = {f"response_{i+1}": pd.read_json(f"{data_path}/{j_file}") for i, j_file in enumerate(listdir(data_path)[:10])}
 
+
+ow_player_dict['response_1']['current_team'][0]
+
+
+
 ow_player_dict['response_1']['role'].value_counts(dropna=False)
 ow_player_dict['response_5']['role'].value_counts(dropna=False)
 ow_player_dict['response_9']['role'].value_counts(dropna=False)
